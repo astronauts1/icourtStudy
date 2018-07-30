9b746eeb1c2450fe14d16259571c8b977b033aa8
NotControl <18236556108@163.com>
2018-07-30 17:08:37 +0800

    初始化

---
+++ src/main/java/com/icourt/study/StudyApplication.java
@@ -0,0 +1,12 @@
+package com.icourt.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+}
